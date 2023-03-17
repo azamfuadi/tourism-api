9dbc43589affcbe7399e05ddf12d8a6ae8301114
azamfuadi <azam.fuadi@aiesec.net>
2023-03-17 07:05:26 +0700

    change db connection to aws

--- app/__init__.py
+++ app/__init__.py
@@ -11,17 +11,17 @@ from config import Config
 from flask import Flask
 
 engine = create_engine(
-    'mysql+mysqlconnector://freedb_alpamon1:S?U5!?#V2jC4shj@sql.freedb.tech:3306', echo=True)
+    'mysql+mysqlconnector://dbmasteruser:oY_w+vBkOJ9wW#qO087d!G>_?7[`X7}]@ls-e43f932a72e8734b573077e2ac603a69c356c1ed.cpobec3ma8he.ap-southeast-1.rds.amazonaws.com:3306', echo=True)
 existing_databases = engine.execute("SHOW DATABASES;")
 # Results are a list of single item tuples, so unpack each tuple
 existing_databases = [d[0] for d in existing_databases]
 
 # Create database if not exists
-if 'freedb_tourism' not in existing_databases:
-    engine.execute("CREATE DATABASE freedb_tourism")
-    print("Created database freedb_tourism")
+if 'tourism' not in existing_databases:
+    engine.execute("CREATE DATABASE tourism")
+    print("Created database tourism")
 
-url = 'mysql+mysqlconnector://freedb_alpamon1:S?U5!?#V2jC4shj@sql.freedb.tech:3306/freedb_tourism'
+url = 'mysql+mysqlconnector://dbmasteruser:oY_w+vBkOJ9wW#qO087d!G>_?7[`X7}]@ls-e43f932a72e8734b573077e2ac603a69c356c1ed.cpobec3ma8he.ap-southeast-1.rds.amazonaws.com:3306/tourism'
 Base = declarative_base()
 mysql_engine = create_engine(url, echo=True)
 session_factory = sessionmaker(bind=mysql_engine)
